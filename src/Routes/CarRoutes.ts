82af731216216c596b7c4f7a05171b2c745d6dfb
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-26 15:19:34 -0300

    Feat: Cria caminho para rota /cars buscando por todos os carros cadastrados e carros por id

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -5,5 +5,6 @@ const carRouter = Router();
 
 carRouter.post('/', (req, res, next) => new CarController(req, res, next).create());
 carRouter.get('/', (req, res, next) => new CarController(req, res, next).getAll());
+carRouter.get('/:id', (req, res, next) => new CarController(req, res, next).getById());
 
 export default carRouter;
