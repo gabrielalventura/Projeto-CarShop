d6c97d66cd9bb6cb386a7c03fa63cd5260ce866b
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-26 14:59:26 -0300

    Create: Cria get para rota /cars listando todos os carros

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -4,5 +4,6 @@ import CarController from '../Controllers/CarController';
 const carRouter = Router();
 
 carRouter.post('/', (req, res, next) => new CarController(req, res, next).create());
+carRouter.get('/', (req, res, next) => new CarController(req, res, next).getAll());
 
 export default carRouter;
