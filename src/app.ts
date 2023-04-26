59ab1e47ff8876c4cb37d88f28d22d08433b275b
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-26 13:58:31 -0300

    Feat: Cria rota /cars que possibilita realizar o cadastro de um carro

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
+import carRouter from './Routes/CarRoutes';
 
 const app = express();
+app.use(express.json());
+app.use(carRouter);
+app.use(ErrorHandler.handle);
 
 export default app;
