1030f446981315dfd8159907cd83e9a0c7e43057
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-27 09:29:24 -0300

    Feat: Adiciona rota /motorcycles na aplicaçã e possibilita o cadastro de uma nova moto

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandler';
 import carRouter from './Routes/CarRoutes';
+import motoRouter from './Routes/MotorcycleRoutes';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', carRouter);
+app.use('/motorcycles', motoRouter);
 app.use(ErrorHandler.handle);
 
 export default app;
