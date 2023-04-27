7184166d647b64c7a70381d51270caa37574c4d9
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-27 09:38:58 -0300

    Feat: Adiciona funções para buscar por id  na camada controller de Motorcycle

--- src/Controllers/MotorcycleController.ts
+++ src/Controllers/MotorcycleController.ts
@@ -3,6 +3,7 @@ import {
   Request,
   Response,
 } from 'express';
+import { isValidObjectId } from 'mongoose';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleService from '../Services/MotorcycleService';
 
@@ -42,6 +43,22 @@ class MotorcycleController {
     try {
       const list = await this.service.getAll();
       return this.res.status(200).json(list);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async getById() {
+    const { id } = this.req.params;
+    try {
+      if (!isValidObjectId(id)) {
+        return this.res.status(422).json({ message: 'Invalid mongo id' });
+      }
+      const motoById = await this.service.getById(id);
+      if (motoById === null) {
+        return this.res.status(404).json({ message: 'Motorcycle not found' });
+      }
+      return this.res.status(200).json(motoById);
     } catch (error) {
       this.next(error);
     }
