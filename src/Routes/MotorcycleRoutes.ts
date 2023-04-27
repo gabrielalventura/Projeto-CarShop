4e1c56f57fb6d4b3cb65d6ef73089a2e0b90aa5a
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-27 12:26:45 -0300

    Feat: Acrescenta na rota metodo put para realizar update de uma moto cadastrada a partir do id

--- src/Routes/MotorcycleRoutes.ts
+++ src/Routes/MotorcycleRoutes.ts
@@ -6,5 +6,6 @@ const motoRouter = Router();
 motoRouter.post('/', (req, res, next) => new MotorcycleController(req, res, next).create());
 motoRouter.get('/', (req, res, next) => new MotorcycleController(req, res, next).getAll());
 motoRouter.get('/:id', (req, res, next) => new MotorcycleController(req, res, next).getById());
+motoRouter.put('/:id', (req, res, next) => new MotorcycleController(req, res, next).updateById());
 
 export default motoRouter;
