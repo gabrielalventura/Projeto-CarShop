453eaf875861d5c9e5fc7f90c86a61dfb809f3e2
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-27 09:39:51 -0300

    Feat: Adiciona rota para listar todas as motos cadastradas

--- src/Routes/MotorcycleRoutes.ts
+++ src/Routes/MotorcycleRoutes.ts
@@ -4,5 +4,6 @@ import MotorcycleController from '../Controllers/MotorcycleController';
 const motoRouter = Router();
 
 motoRouter.post('/', (req, res, next) => new MotorcycleController(req, res, next).create());
+motoRouter.get('/', (req, res, next) => new MotorcycleController(req, res, next).getAll());
 
 export default motoRouter;
